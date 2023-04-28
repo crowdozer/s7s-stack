2870b77c18fad86708355483238ee46472796a45
christian <32648607+crowdozer@users.noreply.github.com>
2023-04-28 13:51:31 -0500

    convert bg to webp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,7 +70,7 @@
 		left: 0;
 		right: 0;
 		bottom: 0;
-		background: url('/images/bg.png') no-repeat center center fixed;
+		background: url('/images/bg.webp') no-repeat center center fixed;
 		background-size: cover;
 		z-index: -1;
 		opacity: 0.23579;
