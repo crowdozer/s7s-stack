35a6c197aec3c53e480d7f651da905182a9fd5e6
christian <32648607+crowdozer@users.noreply.github.com>
2023-04-27 20:14:48 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -68,7 +68,7 @@
 		left: 0;
 		right: 0;
 		bottom: 0;
-		background: url('./images/bg.png') no-repeat center center fixed;
+		background: url('/images/bg.png') no-repeat center center fixed;
 		background-size: cover;
 		z-index: -1;
 		opacity: 0.23579;
